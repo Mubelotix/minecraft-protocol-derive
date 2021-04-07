6b3dd6d56404efe5670c2ea2bc0bde85a8cc564b
Mubelotix <mubelotix@gmail.com>
2021-04-07 14:00:35 +0200

    Add doc and format

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,32 @@
 extern crate proc_macro;
-use proc_macro::{TokenStream, Span};
-use quote::{quote, format_ident};
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Expr, Lit, LitInt};
+use proc_macro::{Span, TokenStream};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, Fields, Lit, LitInt};
 
 #[proc_macro_derive(MinecraftPacket)]
 pub fn minecraft_packet_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let (lifetime_impl, lifetime_struct, lifetime) = match input.generics.lifetimes().collect::<Vec<&_>>() {
-        lifetimes if lifetimes.is_empty() => (quote!{<'a>}, None, None),
-        lifetimes if lifetimes.len() == 1 => {
-            let lifetime = lifetimes[0].lifetime.clone();
-            (quote! {<#lifetime>}, Some(quote! {<#lifetime>}), Some(quote! {#lifetime}))
-        },
-        _ => return quote!(compile_error!("Too many lifetimes");).into()
-    };
- 
+    let (lifetime_impl, lifetime_struct, lifetime) =
+        match input.generics.lifetimes().collect::<Vec<&_>>() {
+            lifetimes if lifetimes.is_empty() => (quote! {<'a>}, None, None),
+            lifetimes if lifetimes.len() == 1 => {
+                let lifetime = lifetimes[0].lifetime.clone();
+                (
+                    quote! {<#lifetime>},
+                    Some(quote! {<#lifetime>}),
+                    Some(quote! {#lifetime}),
+                )
+            }
+            _ => return quote!(compile_error!("Too many lifetimes");).into(),
+        };
+
     let name = input.ident;
 
     let data = match input.data {
         Data::Struct(data) => data,
-        _ => return quote!(compile_error!("Unsupported data structure");).into()
+        _ => return quote!(compile_error!("Unsupported data structure");).into(),
     };
-    
+
     match data.fields {
         Fields::Named(fields) => {
             let fields = fields.named.into_iter().map(|field| field.ident.unwrap());
@@ -56,6 +61,7 @@ pub fn minecraft_packet_derive(input: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn minecraft_enum(attr: TokenStream, input: TokenStream) -> TokenStream {
+    // Collect data
     let argument_type = attr.to_string();
     let representation_type = match argument_type.as_str() == "VarInt" {
         true => "i32".to_string(),
@@ -64,14 +70,16 @@ pub fn minecraft_enum(attr: TokenStream, input: TokenStream) -> TokenStream {
     let representation_ident = format_ident!("{}", representation_type);
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident.clone();
-
     let data = match input.data.clone() {
         Data::Enum(data) => data,
-        _ => return quote!(compile_error!("Unsupported data structure");).into()
+        _ => return quote!(compile_error!("Unsupported data structure");).into(),
     };
+    let unmatched_message = format!(
+        "The {} ID is outside the definition range.",
+        name.to_string()
+    );
 
-    let unmatched_message = format!("The {} ID is outside the definition range.", name.to_string());
-
+    // Analyse enum variants
     let mut variant_name = Vec::new();
     let mut variant_value = Vec::new();
     let mut last_discriminant = 0;
@@ -87,30 +95,31 @@ pub fn minecraft_enum(attr: TokenStream, input: TokenStream) -> TokenStream {
             last_discriminant + 1
         };
         last_discriminant = discriminant;
-        let discriminant = Lit::Int(LitInt::new(&format!("{}{}", discriminant, representation_type), Span::call_site().into()));
+        let discriminant = Lit::Int(LitInt::new(
+            &format!("{}{}", discriminant, representation_type),
+            Span::call_site().into(),
+        ));
         variant_name.push(variant.ident);
         variant_value.push(discriminant);
-    };
+    }
 
+    // Construct the append_minecraft_packet_part method
     let append_implementation = match argument_type.as_str() {
-        "u8" => {
-            quote! {
-                output.push(self as u8);
-                Ok(())
-            }
-        }
-        "VarInt" => {
-            quote! {
-                VarInt(self as i32).append_minecraft_packet_part(output)
-            }
-        }
+        "u8" => quote! {
+            output.push(self as u8);
+            Ok(())
+        },
+        "VarInt" => quote! {
+            VarInt(self as i32).append_minecraft_packet_part(output)
+        },
         _ => quote! {
             (self as #representation_type).append_minecraft_packet_part(output)
         },
     };
 
+    // Construct the build_from_minecraft_packet method
     let build_implementation = match argument_type.as_str() {
-        "VarInt" =>quote! {
+        "VarInt" => quote! {
             let (id, input) = VarInt::build_from_minecraft_packet(input)?;
             let value = match id.0 {
                 #(#variant_value => #name::#variant_name,)*
@@ -128,10 +137,12 @@ pub fn minecraft_enum(attr: TokenStream, input: TokenStream) -> TokenStream {
         },
     };
 
+    // Derive MinecraftPacketPart
     {quote! {
         #[repr(#representation_ident)]
         #input
 
+        #[automatically_derived]
         impl<'a> MinecraftPacketPart<'a> for #name {
             fn append_minecraft_packet_part(self, output: &mut Vec<u8>) -> Result<(), &'static str> {
                 #append_implementation
